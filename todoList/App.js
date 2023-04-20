c405bb75267e97ca581f522a88217da1ee8ddba7
Anish Sinha <22457135+Anish-Sinha@users.noreply.github.com>
2023-04-20 16:55:44 -0700

    created simple Hello World project, only way from here is up

---
+++ todoList/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Aye lezzgo Anish!</Text>
+      <Text>This is working so far.</Text>
+      <Text>Let's make that To Do List boi 💪</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
