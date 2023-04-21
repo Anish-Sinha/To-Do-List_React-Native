5cd72eddc19c9def8364f7bf0ec04fb13d7fef81
Anish Sinha <22457135+Anish-Sinha@users.noreply.github.com>
2023-04-21 04:53:53 -0700

    created initial layout setup for tasks

--- todoList/App.js
+++ todoList/App.js
@@ -2,21 +2,30 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Aye lezzgo Anish!</Text>
-      <Text>This is working so far.</Text>
-      <Text>Let's make that To Do List boi 💪</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+
+        {/*Today's Tasks*/}
+        <View style={styles.tasksWrapper}>
+            <Text style={styles.sectionTitle}>Today's tasks</Text>
+
+            /*Task Items*/
+            <View style={styles.items}>
+                /*This is where the indv tasks will go*/
+            </View>
+
+        </View>
+
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    tasksWrapper: {}, 
+    sectionTitle: {}, 
+    items: {},
 });
