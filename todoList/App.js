17643989e6accb2949accc904622332e46838ca7
Anish Sinha <22457135+Anish-Sinha@users.noreply.github.com>
2023-04-21 05:26:32 -0700

    created a reusable Task component

--- todoList/App.js
+++ todoList/App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
     return (
@@ -12,6 +13,7 @@ export default function App() {
             {/*Task Items*/}
             <View style={styles.items}>
                 {/*This is where the indv tasks will go*/}
+                <Task />
             </View>
 
         </View>
